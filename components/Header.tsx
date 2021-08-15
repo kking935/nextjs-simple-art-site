40a54823f321442624279c67a48137fe75836157
Ken King <kking935@vt.edu>
2021-08-15 15:28:54 -0500

    added logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,24 @@
 import { FC } from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Header: FC = () => {
     return (
-        <header className='w-screen shadow-md'>            
+        <header className='w-screen shadow-md bg-gray-600'>            
             <nav>
-                <ul className='mx-auto flex justify-start items-center max-w-7xl py-6 px-5'>
+                <ul className='mx-auto flex justify-start items-center max-w-7xl py-6 px-5 font-mono'>
                     <li>
-                        <h1 className='font-extrabold text-2xl'>Mario Nino</h1>
+                        <Link href='/'>
+                            <a>
+                                <Image src='/images/logo-cropped.png' width='130px' height='45px' alt='MARIO NIÑO' />
+                            </a>
+                        </Link>
                     </li>
                     <li className='flex-grow' />
                     <li>
                         <Link href='/contacto'>
                             <a className='bg-blue-500 font-bold text-white p-3 rounded-xl shadow-md'>
-                                Contacto
+                                CONTACTO
                             </a>
                         </Link>
                     </li>
