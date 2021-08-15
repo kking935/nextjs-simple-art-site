c496ed11759dfc8afc43c4d6c392f6ba2cbccf32
Ken King <kking935@vt.edu>
2021-08-15 15:28:36 -0500

    added old site images

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
 import type { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+        <link rel="manifest" href="site.webmanifest" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
